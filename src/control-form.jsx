cc7d82dd54920814a49b71cdefde4d27933ede8d
Eres'ko Egor <411@onlinepbx.ru>
2018-11-08 12:58:07 +0500

    add vis-react

---
+++ src/control-form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ControlForm extends React.Component {
+  render () {
+    return (
+      <div className='control-from'>
+        <div>Control form</div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+import './style.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Graph from 'vis-react'
+import data from './data'
 
-ReactDOM.render(<App />, document.getElementById('example-app'))
+class App extends React.Component {
+  render () {
+    return (
+      <div className='main-app'>
+        <Graph
+          graph={data.graph}
+          options={data.options}
+        />
+      </div>
+    )
+  }
+}
 
+const parentElement = document.createElement('div')
+document.body.appendChild(parentElement)
+
+ReactDOM.render(<App />, parentElement)
